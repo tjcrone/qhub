472d7b662ac67fd0af941c394607cea93f357537
Tim Crone <tjcrone@gmail.com>
2022-09-11 07:21:05 -0400

    Have the CI work off of my fork

--- qhub/provider/cicd/common.py
+++ qhub/provider/cicd/common.py
@@ -3,11 +3,11 @@ import os
 
 def pip_install_qhub(qhub_version: str) -> str:
     qhub_gh_branch = os.environ.get("QHUB_GH_BRANCH", None)
-    pip_install = f"pip install qhub=={qhub_version}"
+    pip_install = f"pip install git+https://github.com/tjcrone/qhub.git"
     # dev branches
     if qhub_gh_branch:
         pip_install = (
-            f"pip install git+https://github.com/Quansight/qhub.git@{qhub_gh_branch}"
+            f"pip install git+https://github.com/tjcrone/qhub.git@{qhub_gh_branch}"
         )
 
     return pip_install
